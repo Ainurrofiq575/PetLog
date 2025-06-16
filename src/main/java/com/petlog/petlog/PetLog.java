92339c12d5680d466e856594189cc430afa8c3ec
redmipronot268@gmail.com <redmipronot268@gmail.com>
2025-06-16 10:16:56 +0700

    first commit

---
+++ src/main/java/com/petlog/petlog/PetLog.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.petlog.petlog;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class PetLog {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
